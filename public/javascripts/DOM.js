2622b62a5a7f027718141164c5bc11b4846a4f41
seam <honghaitzz11@gmail.com>
2017-11-07 15:19:19 +0800

    添加了官方示例代码. 生成自己的战舰游戏静态界面

--- public/javascripts/DOM.js
+++ public/javascripts/DOM.js
@@ -1,4 +1,10 @@
 window.onload = () => {
-    console.log('hello');
+    // console.log('hello');
     document.getElementById('patter').innerHTML = 'patter'
+    let pater1 = document.getElementById('pater1');
+    // 添加class属性
+    pater1.setAttribute("class", "redtext");
+    // 获取class属性 :只对 ID 有效，class无效
+    let href = document.getElementById('href');
+    console.log(href.getAttribute('href'));
 }
